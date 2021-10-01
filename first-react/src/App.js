657e6f8002ebe967111e6da3211f92f4b1278913
Daris <daris.hodzic.dh@gmail.com>
2021-10-01 20:53:46 +0200

    init commit

---
+++ first-react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>
+        Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha
+        forage.
+      </h2>
+      <button>Button</button>
+    </div>
+  );
+}
+
+export default App;
